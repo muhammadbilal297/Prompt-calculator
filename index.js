c973d3006b2eb74c3c43f6d8c2ecfcf7951c5e77
muhammadbilal297 <mb973768@gmail.com>
2024-12-26 00:07:22 +0500

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var userName = prompt("Enter your Name")
+alert("Welcome " + userName)
+
+var math = +prompt("Enter your Maths num")
+var bio = +prompt("Enter your Biology num")
+var phy = +prompt("Enter your Physics num")
+var result= math + Biology + phy
+
+alert("Your total number is " + result)
+
